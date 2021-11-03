c7284aadbe162787719f9f58cc48755ffa6d4be9
Annabelle Zhang <annabelle.zhang@education.nsw.gov.au>
2021-11-04 09:13:32 +1100

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,18 @@
+print(" ------------------------------------------------") 
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Annabelle Zhang                      |") 
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+ans = input("What is the name of the subject ")
+while ans != ("IST"):
+    print("Not correct - try again")
+    ans = input("What is the name of the subject ")
+    continue
+else:
+    print("")
+    print("")
+    print("\nCongratulations!!")
+    print("")
+    print("")
